c1f7408d1c5a7c98c0368adfa0ccdf4ea42b80fa
sohail <48204572+sohailkhan24@users.noreply.github.com>
2021-03-31 15:51:51 +0530

    Signup created

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static("./assets"));
 app.use(expressLayouts);
 //extract style and scripts from sub pages into layouts
+
 app.set("layout extractStyles", true);
-app.set("layout extractScript", true);
+app.set("layout extractScripts", true);
 
 //use exports router
 app.use("/", require("./routers"));
--- routers/users.js
+++ routers/users.js
@@ -4,5 +4,9 @@ const users = require("../controllers/users_controller");
 const router = express.Router();
 
 router.get("/profile", users.profile);
+router.get("/sign-up", users.signUp);
+router.get("/sign-in", users.signIn);
+
+router.post("/create", users.create);
 
 module.exports = router;
