79de726c8a16e9b7e4a39d2ff88c8934602142c6
sohail <48204572+sohailkhan24@users.noreply.github.com>
2021-04-02 15:54:21 +0530

    Authenticated post
    Displaying user name while posting and authenticating him

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,9 +1,17 @@
-module.exports.post = function (req, res) {
-  return res.render("posts", {
-    title: "Posts",
-  });
-};
+const Post = require("../models/post");
 
-module.exports.createPost = function (req, res) {
-  //Todo
+module.exports.create = function (req, res) {
+  Post.create(
+    {
+      content: req.body.content,
+      user: req.user._id,
+    },
+    function (err, post) {
+      if (err) {
+        console.log("Error in creaing a post");
+        return;
+      }
+      return res.redirect("back");
+    }
+  );
 };
--- routers/posts.js
+++ routers/posts.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const passport = require("passport");
 const posts = require("../controllers/posts_controller");
 
 const router = express.Router();
 
-router.get("/post", posts.post);
-router.post("/createPosts", posts.createPost);
+// router.get("/post", posts.post);
+router.post("/create", passport.checkAuthentication, posts.create);
 
 module.exports = router;
