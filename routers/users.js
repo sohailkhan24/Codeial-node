01fc8d314f8f007235b9290bc823646056ee8a62
sohail <48204572+sohailkhan24@users.noreply.github.com>
2021-04-03 23:37:25 +0530

    Update Profile

--- routers/users.js
+++ routers/users.js
@@ -9,6 +9,11 @@ router.get(
   passport.checkAuthentication,
   usersController.profile
 );
+router.post(
+  "/update/:id",
+  passport.checkAuthentication,
+  usersController.update
+);
 
 router.get("/sign-up", usersController.signUp);
 router.get("/sign-in", usersController.signIn);
