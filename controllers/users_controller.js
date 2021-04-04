541b91f090dfa6cb5200cc0beed4fd9cd63438a1
sohail <48204572+sohailkhan24@users.noreply.github.com>
2021-04-04 23:56:49 +0530

    flash messages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -62,10 +62,13 @@ module.exports.create = function (req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function (req, res) {
+  req.flash("success", "Logged in Successfully");
   return res.redirect("/");
 };
 
 module.exports.destroySession = function (req, res) {
   req.logout();
+  req.flash("success", "Logged out Successfully");
+
   return res.redirect("/");
 };
--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
 const MongoStore = require("connect-mongo")(session);
 const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
 
 app.use(
   sassMiddleware({
@@ -60,6 +62,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
+app.use(flash());
+app.use(customMware.setFlash);
 //use exports router
 app.use("/", require("./routers"));
 
