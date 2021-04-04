cce096ba8e07a7bf30cd1533e983299a97959e30
sohail <48204572+sohailkhan24@users.noreply.github.com>
2021-04-05 01:15:28 +0530

    deleting by ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,6 +13,7 @@
         success: function (data) {
           let newPost = newPostDom(data.data.post);
           $("#posts-list-container>ul").prepend(newPost);
+          deletePost($(" .delete-post-button", newPost));
         },
         error: function (error) {
           console.log(error.responseText);
@@ -24,34 +25,53 @@
   // method to create a post in DOM
   let newPostDom = function (post) {
     return $(`<li id="post-${post._id}">
-                    <p>
+                  <p>
+                      
+                      <small>
+                          <a class="delete-post-button"  href="/posts/destroy/${post._id}">X</a>
+                      </small>
+                     
+                      ${post.content}
+                      <br>
+                      <small>
+                      ${post.user.name}
+                      </small>
+                  </p>
+                  <div class="post-comments">
+                      
+                          <form action="/comments/create" method="POST">
+                              <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                              <input type="hidden" name="post" value="${post._id}" >
+                              <input type="submit" value="Add Comment">
+                          </form>
+             
+              
+                      <div class="post-comments-list">
+                          <ul id="post-comments-${post._id}">
+                              
+                          </ul>
+                      </div>
+                  </div>
+                  
+              </li>`);
+  };
 
-                        <small>
-                            <a class="delete-post-button"  href="/posts/destroy/${post.id}">X</a>
-                        </small>
+  // method to delete a post from DOM
+  let deletePost = function (deleteLink) {
+    $(deleteLink).click(function (e) {
+      e.preventDefault();
 
-                        ${post.content}
-                        <br>
-                        <small>
-                        ${post.user.name}
-                        </small>
-                    </p>
-                    <div class="post-comments">
-
-                            <form action="/comments/create" method="POST">
-                                <input type="text" name="content" placeholder="Type Here to add comment..." required>
-                                <input type="hidden" name="post" value="${post._id}" >
-                                <input type="submit" value="Add Comment">
-                            </form>
-
-                        <div class="post-comments-list">
-                            <ul id="post-comments-${post._id}">
-
-                            </ul>
-                        </div>
-                    </div>
-
-                </li>`);
+      $.ajax({
+        type: "get",
+        url: $(deleteLink).prop("href"),
+        success: function (data) {
+          $(`#post-${data.data.post_id}`).remove();
+        },
+        error: function (error) {
+          console.log(error.responseText);
+        },
+      });
+    });
   };
 
   createPost();
