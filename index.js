f3523f08eca15a247d4a1e4f1e66449c043c1046
sohail <48204572+sohailkhan24@users.noreply.github.com>
2021-04-05 18:09:19 +0530

    Avatar picuters and comment ajax

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("./assets"));
+//make the uploads path avaiable to browser
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(expressLayouts);
 //extract style and scripts from sub pages into layouts
 
