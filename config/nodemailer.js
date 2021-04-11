484da222182a20261fb7572da9f6f53eb5c56bbe
sohail <48204572+sohailkhan24@users.noreply.github.com>
2021-04-11 21:42:40 +0530

    getting dev ready

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,37 +1,29 @@
 const nodemailer = require("nodemailer");
-const ejs = require('ejs');
-const path = require('path')
+const ejs = require("ejs");
+const path = require("path");
+const env = require("./environment");
 
-
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-        user: 'alchemy.cn18',
-        pass: 'codingninjas'
-    }
-});
-
+let transporter = nodemailer.createTransport(env.smpt);
 
 let renderTemplate = (data, relativePath) => {
-    let mailHTML;
-    ejs.renderFile(
-        path.join(__dirname, '../views/mailers', relativePath),
-        data,
-        function(err, template){
-         if (err){console.log('error in rendering template', err); return}
-         
-         mailHTML = template;
-        }
-    )
+  let mailHTML;
+  ejs.renderFile(
+    path.join(__dirname, "../views/mailers", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("error in rendering template", err);
+        return;
+      }
 
-    return mailHTML;
-}
+      mailHTML = template;
+    }
+  );
 
+  return mailHTML;
+};
 
 module.exports = {
-    transporter: transporter,
-    renderTemplate: renderTemplate
-}
+  transporter: transporter,
+  renderTemplate: renderTemplate,
+};
--- index.js
+++ index.js
@@ -1,76 +1,91 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const logger = require("morgan");
+const env = require("./config/environment");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 // used for session cookie
-const session = require('express-session');
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-const passportJWT = require('./config/passport-jwt-strategy');
-const passportGoogle = require('./config/passport-google-oauth2-strategy');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const passportJWT = require("./config/passport-jwt-strategy");
+const passportGoogle = require("./config/passport-google-oauth2-strategy");
 
-const MongoStore = require('connect-mongo')(session);
-const sassMiddleware = require('node-sass-middleware');
-const flash = require('connect-flash');
-const customMware = require('./config/middleware');
+const MongoStore = require("connect-mongo")(session);
+const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
 
 // setup the chat server to be used with socket.io
-const chatServer = require('http').Server(app);
-const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+const chatServer = require("http").Server(app);
+const chatSockets = require("./config/chat_sockets").chatSockets(chatServer);
 chatServer.listen(5000);
-console.log('chat server is listening on port 5000');
+console.log("chat server is listening on port 5000");
 
-
-app.use(sassMiddleware({
-    src: './assets/scss',
-    dest: './assets/css',
+const path = require("path");
+// if(env.name == 'development'){
+//   app.use(
+//     sassMiddleware({
+//       src: path.join(__dirname, env.asset_path, "scss"),
+//       dest: path.join(__dirname, env.asset_path, "css"),
+//       debug: true,
+//       outputStyle: "extended",
+//       prefix: "/css",
+//     })
+//   );
+// }
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, env.asset_path, "scss"),
+    dest: path.join(__dirname, env.asset_path, "css"),
     debug: true,
-    outputStyle: 'extended',
-    prefix: '/css'
-}));
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-app.use(express.static('./assets'));
+app.use(express.static(env.asset_path));
 // make the uploads path available to the browser
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+// app.use(logger(env.morgan.mode, env.morgan.options));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
-
-
-
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // mongo store is used to store the session cookie in the db
-app.use(session({
-    name: 'codeial',
+app.use(
+  session({
+    name: "codeial",
     // TODO change the secret before deployment in production mode
-    secret: 'blahsomething',
+    secret: env.session_cookie_key,
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: (1000 * 60 * 100)
+      maxAge: 1000 * 60 * 100,
     },
     store: new MongoStore(
-        {
-            mongooseConnection: db,
-            autoRemove: 'disabled'
-        
-        },
-        function(err){
-            console.log(err ||  'connect-mongodb setup ok');
-        }
-    )
-}));
+      {
+        mongooseConnection: db,
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect-mongodb setup ok");
+      }
+    ),
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -81,13 +96,12 @@ app.use(flash());
 app.use(customMware.setFlash);
 
 // use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
 
-app.listen(port, function(err){
-    if (err){
-        console.log(`Error in running the server: ${err}`);
-    }
-
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
